dcc0267df13038e002ce1344a925d0c8304f4467
nobodyman <a@sibns.ru>
2017-03-29 13:12:38 +0700

    2.0.4

--- src/ChartJs.php
+++ src/ChartJs.php
@@ -94,47 +94,7 @@ class ChartJs extends Widget
         $options = !empty($this->clientOptions) ? Json::encode($this->clientOptions) : '{}';
 
         $js = ";var chartJS_{$id} = new Chart(document.getElementById('{$id}').getContext('2d'), {'type': '{$type}', 'data': {$data}, 'options': {$options}});";
-//        $js = ";var chartJS_{$id} = new Chart(document.getElementById('{$id}').getContext('2d'),$config);";
         $view->registerJs($js);
     }
 
-    protected function json_encode_advanced(array $arr, $sequential_keys = false, $quotes = false, $beautiful_json = false)
-    {
-
-        $output = "{";
-        $count  = 0;
-        foreach ($arr as $key => $value) {
-
-            if ($this->isAssoc($arr) || (!$this->isAssoc($arr) && $sequential_keys == true)) {
-                $output .= ($quotes ? '"' : '') . $key . ($quotes ? '"' : '') . ': ';
-            }
-
-            if (is_array($value)) {
-                $output .= $this->json_encode_advanced($value, $sequential_keys, $quotes, $beautiful_json);
-            } else if (is_bool($value)) {
-                $output .= ($value ? 'true' : 'false');
-            } else if (is_numeric($value)) {
-                $output .= $value;
-            } else {
-                $output .= ($quotes || $beautiful_json ? '"' : '') . $value . ($quotes || $beautiful_json ? '"' : '');
-            }
-
-            if (++$count < count($arr)) {
-                $output .= ', ';
-            }
-        }
-
-        $output .= "}";
-
-        return $output;
-    }
-
-    protected function isAssoc(array $arr)
-    {
-        if (array() === $arr) {
-            return false;
-        }
-
-        return array_keys($arr) !== range(0, count($arr) - 1);
-    }
 }
